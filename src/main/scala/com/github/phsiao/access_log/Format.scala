774b6bb64e8ef2fce73275f3ea82e05755eb01ce
Shawn Hsiao <shawn.hsiao@gmail.com>
2011-11-29 15:27:45 -0500

    support <format>_as_map

--- src/main/scala/com/github/phsiao/access_log/Format.scala
+++ src/main/scala/com/github/phsiao/access_log/Format.scala
@@ -1,6 +1,19 @@
 package com.github.phsiao.access_log.Format
 
 import com.github.phsiao.access_log.Util._
+import scala.collection.immutable.ListMap
+
+object field {
+  val REMOTEHOST  = "REMOTEHOST"
+  val IDENTITY    = "IDENTITY"
+  val USERID      = "USERID"
+  val TIMESTAMP   = "TIMESTAMP"
+  val REQUEST     = "REQUEST"
+  val STATUS      = "STATUS"
+  val SIZE        = "SIZE"
+  val REFERRER    = "REFERRER"
+  val USERAGENT   = "USERAGENT"
+}
 
 /** Parse Apache common log format
   *
@@ -11,9 +24,19 @@ import com.github.phsiao.access_log.Util._
 object common {
   val le = """(.+) (.+) (.+) \[(.+)\] "(.*)" (\d+) ([-\d]+)""".r
   def unapply(line: String) = {
-    val le(clf_string(hostname), clf_string(identity), clf_string(userid),
+    val le(clf_string(remotehost), clf_string(identity), clf_string(userid),
            format_t(timestamp), request, format_s(status), format_b(size)) = line
-    Some(hostname, identity, userid, timestamp, request, status, size)
+    Some(remotehost, identity, userid, timestamp, request, status, size)
+  }
+}
+
+object common_as_map {
+  def unapply(line: String) = {
+    val common(remotehost, identity, userid, timestamp, request, status, size) = line
+    import field._
+    Some(ListMap[String, Any](REMOTEHOST -> remotehost, IDENTITY -> identity, USERID -> userid,
+                 TIMESTAMP -> timestamp, REQUEST -> request,
+                 STATUS -> status, SIZE -> size))
   }
 }
 
@@ -26,9 +49,21 @@ object common {
 object combined {
   val le = """(.+) (.+) (.+) \[(.+)\] "(.*)" (\d+) ([-\d]+) "(.*)" "(.*)"""".r
   def unapply(line: String) = {
-    val le(clf_string(hostname), clf_string(identity), clf_string(userid),
+    val le(clf_string(remotehost), clf_string(identity), clf_string(userid),
            format_t(timestamp), request, format_s(status), format_b(size),
            referrer, useragent) = line
-    Some(hostname, identity, userid, timestamp, request, status, size, referrer, useragent)
+    Some(remotehost, identity, userid, timestamp, request, status, size, referrer, useragent)
+  }
+}
+
+object combined_as_map {
+  def unapply(line: String) = {
+    val combined(remotehost, identity, userid, timestamp, request,
+                 status, size, referrer, useragent) = line
+    import field._
+    Some(ListMap[String, Any](REMOTEHOST -> remotehost, IDENTITY -> identity, USERID -> userid,
+                 TIMESTAMP -> timestamp, REQUEST -> request,
+                 STATUS -> status, SIZE -> size,
+                 REFERRER -> referrer, USERAGENT -> useragent))
   }
 }
--- src/test/scala/com/github/phsiao/access_log/FormatTest.scala
+++ src/test/scala/com/github/phsiao/access_log/FormatTest.scala
@@ -3,6 +3,7 @@ package com.github.phsiao.access_log.Format.test
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import com.github.phsiao.access_log.Format._
+import com.github.phsiao.access_log.Format.field._
 
 class FormatTest extends FunSuite with ShouldMatchers {
 
@@ -19,6 +20,18 @@ class FormatTest extends FunSuite with ShouldMatchers {
     size should be (2326)
   }
 
+  test("common_as_map simple example") {
+    val common_as_map(entry) = """127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326"""
+
+    entry(REMOTEHOST) should be ("127.0.0.1")
+    entry(IDENTITY) should be ("")
+    entry(USERID) should be ("frank")
+    entry(TIMESTAMP) should be (971211336)
+    entry(REQUEST) should be ("GET /apache_pb.gif HTTP/1.0")
+    entry(STATUS) should be (200)
+    entry(SIZE) should be (2326)
+  }
+
   test("combined simple example") {
     val combined(hostname, identity, userid, timestamp, request, status, size, referrer, useragent) =
       """127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)""""
@@ -32,5 +45,20 @@ class FormatTest extends FunSuite with ShouldMatchers {
     size should be (2326)
     referrer should be ("http://www.example.com/start.html")
     useragent should be ("Mozilla/4.08 [en] (Win98; I ;Nav)")
+  }
+
+  test("combined_as_map simple example") {
+    val combined_as_map(entry) =
+      """127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)""""
+
+    entry(REMOTEHOST) should be ("127.0.0.1")
+    entry(IDENTITY) should be ("")
+    entry(USERID) should be ("frank")
+    entry(TIMESTAMP) should be (971211336)
+    entry(REQUEST) should be ("GET /apache_pb.gif HTTP/1.0")
+    entry(STATUS) should be (200)
+    entry(SIZE) should be (2326)
+    entry(REFERRER) should be ("http://www.example.com/start.html")
+    entry(USERAGENT) should be ("Mozilla/4.08 [en] (Win98; I ;Nav)")
   }
 }
