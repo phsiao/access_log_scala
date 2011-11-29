10aad9b9f1678a5b1c3de70d4cf60beccec71f2a
Shawn Hsiao <shawn.hsiao@gmail.com>
2011-11-28 22:06:10 -0500

    add scaladoc

--- src/main/scala/com/github/phsiao/access_log/Util.scala
+++ src/main/scala/com/github/phsiao/access_log/Util.scala
@@ -1,5 +1,12 @@
 package com.github.phsiao.access_log.Util
 
+/** Some utilities for parsing directives.
+  *
+  * http://httpd.apache.org/docs/2.2/mod/mod_log_config.html#formats
+  */
+
+/** Parse directive %b as Integer
+  */
 object directive_b {
   def unapply(s: String): Option[Int] = {
     if (s == "-") Some(0)
